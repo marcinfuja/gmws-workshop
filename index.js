6ab2276f6c80f87c976831b0dfb9d1800b80e562
Marcin Fuja <mfuja@pgs-soft.com>
2019-03-07 16:39:50 +0100

    service worker finished

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 (function(){
-    console.log('test')
-})()
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function(){
+            navigator.serviceWorker.register('/sw.js')
+                .then(function(registration) {
+                    console.log('Service Worker registration successful with scope: ', registration.scope)
+                }, function(err){
+                    console.log(err)
+                })
+        });
+    }
+})();
