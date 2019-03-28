50afef02a18b08758bfe816dbf519303e02e3f57
Marcin Fuja <mfuja@pgs-soft.com>
2019-03-28 13:49:55 +0100

    clear old sw data; add test data

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 (function(){
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function(){
-            navigator.serviceWorker.register('/sw.js')
-                .then(function(registration) {
-                    console.log('Service Worker registration successful with scope: ', registration.scope)
-                }, function(err){
-                    console.log(err)
-                })
-        });
-    }
+    'use strict';
+
+    const customerData = [
+        { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+        { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+      ];
+
+    console.log('Cache or not to cache?')
+      
 })();
