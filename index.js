56a031e4f087ff02cd469f35a730d6315ed5dbe8
Marcin Fuja <mfuja@pgs-soft.com>
2019-04-04 14:33:08 +0200

    new stuff

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 (function(){
-    'use strict';
+  'use strict';
 
-    const customerData = [
-        { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-        { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
-      ];
+  const itemsToAdd = [
+    {
+      name: 'banana',
+      price: '$2.99',
+      description: 'It is a yellow banana!',
+      created: new Date().getTime()
+    },
+    {
+      name: 'apple',
+      price: '$4.99',
+      description: 'It is a red apple!',
+      created: new Date().getTime()
+    },
+    {
+      name: 'orange',
+      price: '$0.99',
+      description: 'It is an orange orange!',
+      created: new Date()
+    }
+  ]
+  const addItemInput = document.getElementById('input-add')
+  const addItemButton = document.getElementById('btn-add')
+  const getItemButton = document.getElementById('btn-read')
+  const removeItemInput = document.getElementById('input-remove')
+  const removeItemButton = document.getElementById('btn-remove')
+  const displayResultElement = document.getElementById('table-body')
+  
+  let db;
 
-    console.log('Cache or not to cache?')
-      
+  addItemButton.addEventListener('click', function(){
+    const itemToAdd = addItemInput.value
+    addItem(itemToAdd);
+  });
+  getItemButton.addEventListener('click', function(){
+    getItem();
+  });
+  removeItemButton.addEventListener('click', function(){
+    const itemToRemove = removeItemInput.value;
+    removeItem(itemToRemove);
+  });
+
+  function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+      if (myArray[i].name === nameKey) {
+          return myArray[i];
+      }
+    }
+  }
+
+  //
+
 })();
